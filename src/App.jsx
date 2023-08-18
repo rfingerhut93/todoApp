b79a44a31104c596d1b71a74c6e1b96c49056e6d
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-08-18 16:57:51 -0700

    Add event listener for local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import "./styles.css"
 import { NewTodoForm } from './NewTodoForm';
 import { TodoList } from './TodoList';
 
 export default function App(){
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue);
+  })
+
+
+  /* Run everytime todos changes and store in local storage. */
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title){
     setTodos((currentTodos) => {
